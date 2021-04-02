137524938c30d27046537f869eb8cb6bd0df2a54
Chintan <chihempat@gmail.com>
2021-04-03 02:26:26 +0530

    added multer

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var path = require('path');
 const express = require('express')
 const fs = require('fs')
 const multer = require('multer')
-    //const xlsx = require('xlsx');
-    //const pathxlsxFile = require('read-excel-file/node')
+const xlsx = require('xlsx');
+const xlsxFile = require('read-excel-file/node')
 const mongoose = require('mongoose');
-//const excelToJson = require('convert-excel-to-json');
+const excelToJson = require('convert-excel-to-json');
 const { MongoClient } = require("mongodb");
 //const csv = require('csv-parser');
 const CSVToJSON = require('csvtojson');
@@ -36,7 +36,7 @@ var storage = multer.diskStorage({
         cb(null, 'uploads')
     },
     filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, file.fieldname + '-' + Date.now() + "." + file.originalname.split('.')[1])
     }
 })
 
@@ -44,6 +44,7 @@ var upload = multer({ storage: storage })
 
 
 //app.use(pdf);
+
 app.use(express.urlencoded({ extended: false })); //handle body requests
 app.use(express.json()); // let's make JSON work too!
 app.use('/', express.static(__dirname + '/public'));
@@ -54,16 +55,77 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render('index')
-})
-app.post('/convert', upload.single('file'), (req, res) => {
-    console.log("coverted")
+        res.render('index')
+    })
+    // app.post('/convert', upload.single('file'), (req, res) => {
+
+//     console.log("coverted")
+// })
+
+
+app.post("/convert", upload.single('file'), async(req, res) => {
+    var ext = req.file.originalname.split('.')[1];
+    var og = req.file.originalname.split('.')[0];
+
+    if (ext == "xlsx") {
+        console.log(1)
+
+        console.log(2)
+        try {
+            console.log(3)
+            var data = excelToJson({
+                source: fs.readFileSync(req.file.path),
+                columnToKey: {
+                    '*': '{{columnHeader}}'
+                }
+            });
+            data = data.Sheet1
+            const database = client.db("filesdb");
+            const db = database.collection(og);
+            const options = { ordered: true };
+            console.log(4)
+            const result = await db.insertMany(data, options);
+            console.log(`${result.insertedCount} documents were inserted`);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            console.log("done")
+            res.redirect('/')
+        }
+
+    } else if (ext == "csv") {
+        console.log(5)
+
+        console.log(6)
+        try {
+            console.log(7)
+            const data = await CSVToJSON().fromFile('uploads/grades.csv');
+            const database = client.db("filesdb");
+            const db = database.collection(og);
+            const options = { ordered: true };
+            console.log(8)
+            const result = await db.insertMany(data, options);
+            console.log(`${result.insertedCount} documents were inserted`);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            console.log("done")
+            res.redirect('/')
+        }
+    }
+
 })
 
 
-app.get("/csvtopdf", async(req, res) => {
-    const data = await CSVToJSON().fromFile('uploads/grades.csv');
-    xlpdf(req, res, data, "template.ejs")
+app.get("/csvtopdf", upload.single('file'), async(req, res) => {
+    var keys = [];
+    console.log(req.file)
+        // const data = await CSVToJSON().fromFile(req.file.path);
+        // Object.keys(data[0]).forEach(function(key) {
+        //     var value = data[0][key];
+        //     keys.push(value)
+        // });
+        // topdf(req, res, data, keys);
 })
 
 
@@ -71,35 +133,21 @@ app.get("/csvtopdf", async(req, res) => {
 
 app.get("/xltopdf", upload.single('file'), (req, res) => {
     console.log(req.body.file)
-
-    if (fs.existsSync('./product.json')) {
-        let rawdata = fs.readFileSync('product.json');
-        let data = JSON.parse(rawdata);
-        xlpdf(req, res, data, "template2.ejs");
-        // console.log(data);
-    } else {
-        node_xj({
-                input: "./uploads/Product.xlsx", // input xls
-                output: "product.json", // output json
-                allowEmptyKey: false,
-                lowerCaseHeaders: true
-            },
-            function(err, result) {
-                if (err) {
-                    console.error(err);
-                } else {
-                    let rawdata = fs.readFileSync('product.json');
-                    let data = JSON.parse(rawdata);
-                    xlpdf(req, res, data, "template2.ejs");
-                }
-            }
-        );
-    }
+    var keys = [];
+    var data = excelToJson({
+        source: fs.readFileSync(req.file.path),
+        columnToKey: {
+            '*': '{{columnHeader}}'
+        }
+    });
+    data = data.Sheet1;
+    Object.keys(data[0]).forEach(function(key) {
+        var value = data[0][key];
+        keys.push(value)
+    });
+    console.log(keys)
+    topdf(req, res, data, keys);
 })
-
-// console.log(data);
-
-
 
 app.get("/excelltojson", (req, res) => {
 
@@ -136,12 +184,10 @@ app.get("/excelltojson", (req, res) => {
 })
 
 app.get("/csvtojson", (req, res) => {
-
     async() => {
         try {
-            await client.connect();
             const database = client.db("filesdb");
-            const grades = database.collection("products");
+            const grades = database.collection("grades");
             const options = { ordered: true };
             const result = await grades.insertMany(xl, options);
             console.log(`${result.insertedCount} documents were inserted`);
@@ -152,22 +198,15 @@ app.get("/csvtojson", (req, res) => {
 
 })
 
-
-
-
-
-
-
-
 app.listen(PORT, () => {
     console.log('Mongoose listening on port ' + PORT);
 })
 
 
 
-var xlpdf = function(req, res, data, h, w) {
+var topdf = function(req, res, data, keys) {
 
-    ejs.renderFile(path.join(__dirname, './views/', "template2.ejs"), { result: data, route: "xlsx" }, (err, data) => {
+    ejs.renderFile(path.join(__dirname, './views/', "data.ejs"), { result: data, keys: keys }, (err, html) => {
         if (err) {
             res.send(err);
         } else {
@@ -181,7 +220,7 @@ var xlpdf = function(req, res, data, h, w) {
                     "height": "20mm",
                 },
             };
-            pdf.create(data, options).toFile("report.pdf", function(err, data) {
+            pdf.create(html, options).toFile("report.pdf", function(err, data) {
                 if (err) {
                     res.send(err);
                 } else {
